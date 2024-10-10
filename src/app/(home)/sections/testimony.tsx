4fb0ca2d277e3266f6655266ea46ff7d22255fce
McMaina <irutujmaina@gmail.com>
2024-10-10 19:11:20 +0300

    updated testimonials

--- src/app/(home)/sections/testimony.tsx
+++ src/app/(home)/sections/testimony.tsx
@@ -60,56 +60,56 @@ const testimonials = [
     id: 1,
     img: "/avatar.jpeg",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatum.",
-    name: "John Doe",
+      "Congratulation for the job well done, Rongai Soccer is surely looking forward to the next edition, thank you!",
+    name: "Rongai Soccer Academy",
   },
   {
     id: 2,
     img: "/avatar.jpeg",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatum.",
-    name: "Jane Doe",
+      "Vipawa ladies would like to thank the management and all the ladies teams that took part in the tournament. We enjoyed every bit and made alot of friends. Kazi safi Akumu Kevin and your entire team",
+    name: "Vipawa Ladies",
   },
   {
     id: 3,
     img: "/avatar.jpeg",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatum.",
-    name: "Michael Doe",
+      "The entire organization team, you did an amazing job. Soka Talent had fun, interacted and most impotantly built long-lasting relationships with like-minded people. We can't wait for the next edition. Asanteeni sana.",
+    name: "Soka Talent Academy",
   },
   {
     id: 4,
     img: "/avatar.jpeg",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatum.",
-    name: "Jones Doe",
+      "Excellent work to the organisers and your team. You did an amazing job! All those who participated are winners. We all took a lesson home, areas that we should improve and also recognized some strengths we are not aware of. Be blessed, and i can't wait for the next edition.",
+    name: "Decathlon Kenya",
   },
   {
     id: 5,
     img: "/avatar.jpeg",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatum.",
-    name: "Smith Doe",
+      "It was a fantastic experience for all our teams and we truly appreciate the effort and dedication that went into making it a success. We are already looking forward to the 2nd edition. Thank you for creating unforgettable memories for our young players.",
+    name: "Passion Soccer Academy",
   },
   {
     id: 6,
     img: "/avatar.jpeg",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatum.",
-    name: "Hellen Doe",
+      "Phoenix Soccer Academy takes this opportunity to thank the NIC team for a great tournament. That you could disqualify a team for age cheating at whatever level deserves great respect as it is levels the playing field and gives chance to those young ones whose spaces are always taken up by bigger players. We can't wait for the next one.",
+    name: "Phoenix Soccer Academy",
   },
   {
     id: 7,
     img: "/avatar.jpeg",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatum.",
-    name: "Mary Doe",
+      "A big congratulations to Akumu Kevin and his organising committee. You have done well for a 1st edition and we now that the sky is not the limit for you! So much went well and the many levels of awards made it worth it. The players are happy and that makes us happy. Cheers and looking forward to the next edition.",
+    name: "Sakata Soka Stars",
   },
   {
     id: 8,
     img: "/avatar.jpeg",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatum.",
-    name: "James Doe",
+      "As Express Academy, we take this opportunity to thank the NIC team for organizing the tournament. There were few challenges which I believe the management will look into to make it better next time. Mistaes are the portals of discovery, I believe you have discovered something that will make it better next time.",
+    name: "Express Soccer Academy",
   },
 ];
